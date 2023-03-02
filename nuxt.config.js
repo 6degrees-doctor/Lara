16e936b9676ae61d9843d717ffd48d5e13644e52
LaraAlsaqour <124499194+LaraAlsaqour@users.noreply.github.com>
2023-03-02 17:00:43 +0300

    changing css links

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,14 +10,8 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/ProRate.png' },
-    {rel:'stylesheet', href:'https://fonts.googleapis.com/css2?family=Cinzel&display=swap"'},
-    {rel:'stylesheet', href:'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'},
-    {rel:'stylesheet', href:'https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css'},
-    {rel:'stylesheet',href:'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'},
-    {rel:'stylesheet', href:'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css'}
-  ] ,
+      link: [  { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css' }
+    ],
   script:[{
     src:'/JS/bootstrap.min.js',
    },
