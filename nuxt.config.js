cdd6b843fd0f7fb62e4549df520cb8e29c8b4182
LaraAlsaqour <124499194+LaraAlsaqour@users.noreply.github.com>
2023-03-03 17:07:29 +0300

    changing css links

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-      link: [  { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css' }
+      link: [    { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css' }
     ],
   script:[{
     src:'/JS/bootstrap.min.js',
