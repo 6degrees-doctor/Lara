3e70d2faa2a84076b4ae9694603a82d78eb857ef
LaraAlsaqour <124499194+LaraAlsaqour@users.noreply.github.com>
2023-03-05 11:33:22 +0300

    Fixing navbar issue

--- components/navb.vue
+++ components/navb.vue
@@ -4,7 +4,7 @@
             <div class="container px-4 px-lg-5">
                 <a href="/"><img class="navbar-brand" src="~/assets/img/ProRate.png" width="18%" height="18%"></a>
                 <div id="myNav" class="overlay">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="javascript:void(0)" class="closebtn" @click="closeNav()">&times;</a>
                 <div class="overlay-content">
                    <nuxt-link class="nav-link" target="_blank" to="/">Home</nuxt-link>
                     <nuxt-link class="nav-link" to="#about">About</nuxt-link>
@@ -14,9 +14,23 @@
                     <nuxt-link class="nav-link" target="_blank" to="signDoctor">Sign as Doctor</nuxt-link>
                 </div>
                 </div>
-                <span style="font-size:40px;cursor:pointer" onclick="openNav()">&#9776;</span>
+                <span style="font-size:40px;cursor:pointer" @click="openNav()">&#9776;</span>
             </div>
         </nav>
 </div>
 </template>
 
+<script>
+
+export default {
+    methods: {
+        openNav() {
+            document.getElementById("myNav").style.height = "100%";
+        },
+        closeNav() {
+            document.getElementById("myNav").style.height = "0%";
+        }
+    }
+}
+
+</script>
